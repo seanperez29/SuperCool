b1f321a5da10fa5ab7f68c33d7199223fb8cf115
Sean Perez <seanperez@Seans-MBP.attlocal.net>
2016-04-11 04:12:49 -0700

    Added files for SuperCool

---
+++ SuperCool/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by Sean Perez on 4/10/16.
+//  Copyright © 2016 Awesom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var coolLogo: UIImageView!
+    @IBOutlet weak var coolBg: UIImageView!
+    @IBOutlet weak var uncoolButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func makeMeSuperCool(sender: AnyObject) {
+        coolLogo.hidden = false
+        coolBg.hidden = false
+        uncoolButton.hidden = true
+    
+    }
+
+}
+
